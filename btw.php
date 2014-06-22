d26b9beb5d9a61e1cd622cbde7bb6acb44f5a9dc
Bart van Dorp (iMac) <bvandorp@iMac.local>
2014-06-22 22:21:53 +0200

    Initial file

---
+++ btw.php
@@ -0,0 +1,18 @@
+<?php
+//output modifier to calculate percentage tax by given amount
+//useage: [[+btwvalue:btw=`21`]]
+
+$defaultbtw = 21;
+
+$total = $input;
+
+if ( true === isset($options) ) {
+	$btw = $options;
+} else {
+	$btw = $defaultbtw;
+}
+
+$btw_calc = $btw+100;
+$result = $total/$btw_calc*$btw;
+
+return $result;
